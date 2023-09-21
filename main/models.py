4a1fa2ab5a3ab42d578dfc175ed8b8f901ae7c9a
samuelcodingjourney <gregoriussamuelh@gmail.com>
2023-09-21 10:23:04 +0700

    assignment 4 commit

--- main/models.py
+++ main/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     amount = models.IntegerField()
     description = models.TextField()
-
-
     #date_added = models.DateField(auto_now_add=True)
